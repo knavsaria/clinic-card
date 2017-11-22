ac401ae2e1fd5f387f6b578a491bea4bffe0072d
Keeran Navsaria <keeran.navsaria@gmail.com>
2017-11-22 12:38:42 +0200

    added fragment_children_list

---
+++ app/src/main/java/com/navsaria/keeran/clinicbook/ChildrenActivity.java
@@ -0,0 +1,13 @@
+package com.navsaria.keeran.clinicbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChildrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
