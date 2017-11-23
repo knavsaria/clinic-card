09533013cc539a7fd86bc6e2da9f99b9af11585b
Keeran Navsaria <keeran.navsaria@gmail.com>
2017-11-23 11:51:35 +0200

    added child model

---
+++ app/src/main/java/com/navsaria/keeran/clinicbook/Child.java
@@ -0,0 +1,37 @@
+package com.navsaria.keeran.clinicbook;
+
+/**
+ * Created by keeran on 2017/11/23.
+ */
+
+public class Child {
+
+    private String mFirstName;
+    private String mLastName;
+    private boolean mIsBoy;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+    }
+
+    public boolean isBoy() {
+        return mIsBoy;
+    }
+
+    public void setBoy(boolean boy) {
+        mIsBoy = boy;
+    }
+
+}
--- app/src/main/java/com/navsaria/keeran/clinicbook/ChildrenListFragment.java
+++ app/src/main/java/com/navsaria/keeran/clinicbook/ChildrenListFragment.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by keeran on 2017/11/22.
  */
 
 public class ChildrenListFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
