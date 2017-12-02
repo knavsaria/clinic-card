a18512d52d3ed83e3aabc31bf170d4fa64db7991
Keeran Navsaria <keeran.navsaria@gmail.com>
2017-12-03 00:44:54 +0200

    initial commit

--- app/src/main/java/com/navsaria/keeran/clinicbook/PersonalDataFragment.java
+++ app/src/main/java/com/navsaria/keeran/clinicbook/PersonalDataFragment.java
@@ -6,15 +6,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.UUID;
 
 /**
@@ -28,13 +35,11 @@ public class PersonalDataFragment extends Fragment {
     private static final int REQUEST_CODE = 0;
     public static final String INSTANCE_STATE_CHILD = "com.navsaria.keeran.mchild";
 
-    private Button mDatePicker;
+    private RecyclerView mRecyclerView;
     private Child mChild;
-    private EditText mChildFirstName;
-    private EditText mChildSurname;
-    private EditText mChildIdNum;
-    private EditText mMotherIdNum;
-    private Spinner mNumberOfBirths;
+    private final LinkedHashMap mLinkedHashMap = new LinkedHashMap();
+
+
 
 
     public static PersonalDataFragment newInstance(UUID childId) {
@@ -50,6 +55,9 @@ public class PersonalDataFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLinkedHashMap.put(0, R.layout.personal_data_child_info);
+        mLinkedHashMap.put(1, R.layout.personal_data_mother_info);
+
         if (savedInstanceState != null) {
             mChild = (Child) savedInstanceState.getSerializable(INSTANCE_STATE_CHILD);
         } else {
@@ -69,53 +77,81 @@ public class PersonalDataFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_personal_data, container, false);
-        mDatePicker = (Button) v.findViewById(R.id.dob_child_button);
-        updateDate();
-        mDatePicker.setOnClickListener(new View.OnClickListener() {
+                ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(mChild.getFirstName() + " " + mChild.getLastName());
 
-            @Override
-            public void onClick(View view) {
-                FragmentManager fm = getFragmentManager();
-                DatePickerFragment dialog = DatePickerFragment.newInstance(mChild.getDob());
-                dialog.setTargetFragment(PersonalDataFragment.this, 0);
-                dialog.show(fm, DIALOG_DATE);
-            }
-        });
-
-        mChildFirstName = (EditText) v.findViewById(R.id.edit_text_child_firstname);
-        mChildFirstName.setText(mChild.getFirstName());
-
-        mChildSurname = (EditText) v.findViewById(R.id.edit_text_child_surname);
-        mChildSurname.setText(mChild.getLastName());
-
-        mChildIdNum = (EditText) v.findViewById(R.id.edit_text_child_id);
-
-
-        mMotherIdNum = (EditText) v.findViewById(R.id.edit_text_mother_id);
-
-        mNumberOfBirths = (Spinner) v.findViewById(R.id.number_of_births);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.string_numbers_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mNumberOfBirths.setAdapter(adapter);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.child_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(new PersonalDataAdapter());
 
         return v;
     }
 
-    private void updateDate() {
-        mDatePicker.setText(mChild.getDob().toString());
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != Activity.RESULT_OK) {
-            return;
+    private class PersonalDataAdapter extends RecyclerView.Adapter<PersonalDataHolder> {
+
+
+        @Override
+        public PersonalDataHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new PersonalDataHolder(inflater, parent, viewType);
         }
 
-        if (requestCode == REQUEST_CODE) {
-            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
-            mChild.setDob(date);
-            updateDate();
+        @Override
+        public void onBindViewHolder(PersonalDataHolder holder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return mLinkedHashMap.size();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return (Integer) mLinkedHashMap.get(position);
         }
     }
+
+    private class PersonalDataHolder extends RecyclerView.ViewHolder {
+
+        public PersonalDataHolder(LayoutInflater inflater, ViewGroup parent, int layoutId) {
+            super(inflater.inflate(layoutId, parent, false));
+        }
+    }
+
+    private class PersonalDataChildHolder extends PersonalDataHolder {
+
+        private final String DATE_PICKER_TAG = "com.navsaria.keeran.child.dataPicker";
+
+        private DatePicker mDatePicker;
+
+        public PersonalDataChildHolder(LayoutInflater inflater, ViewGroup parent, int layoutId) {
+            super(inflater, parent, layoutId);
+            mDatePicker = (DatePicker) itemView.findViewById(R.id.dob_child_button);
+            mDatePicker.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    FragmentManager fm = getFragmentManager();
+                    DatePickerFragment datePickerFragment = DatePickerFragment.newInstance(mChild.getDob());
+                    datePickerFragment.show(fm, DATE_PICKER_TAG);
+                }
+            });
+        }
+        private void updateDate() {
+            mDatePicker.setText(mChild.getDob().toString());
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            }
+
+            if (requestCode == REQUEST_CODE) {
+                Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+                mChild.setDob(date);
+                updateDate();
+            }
+        }
+    }
+
 }
